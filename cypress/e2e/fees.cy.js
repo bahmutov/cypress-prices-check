42874d85dbba73e35133b41510c0b7cd5714987b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-03-05 14:22:50 -0500

    more experiments

--- cypress/e2e/fees.cy.js
+++ cypress/e2e/fees.cy.js
@@ -3,6 +3,9 @@
 // https://github.com/bahmutov/cypress-map
 import 'cypress-map'
 
+// https://github.com/bahmutov/cy-spok
+import spok from 'cy-spok'
+
 beforeEach(() => {
   cy.visit('index.html')
 })
@@ -37,4 +40,83 @@ it('shows the expected fees (one array)', () => {
   cy.getInOrder(...selectors)
     .map('innerText')
     .should('deep.equal', ['$10.99', '$6.99', '$1.39', '$1.99', '$21.36'])
+})
+
+it('shows the expected fees (one object)', () => {
+  const names = ['price', 'shipping', 'handling', 'tips', 'total']
+  const selectors = names.map((name) => `[data-cy=${name}] dd`)
+  cy.getInOrder(...selectors)
+    .map('innerText')
+    .apply((values) => Cypress._.zipObject(names, values))
+    .should('deep.equal', {
+      price: '$10.99',
+      shipping: '$6.99',
+      handling: '$1.39',
+      tips: '$1.99',
+      total: '$21.36',
+    })
+})
+
+it.skip('has wrong fees', () => {
+  const names = ['price', 'shipping', 'handling', 'tips', 'total']
+  const selectors = names.map((name) => `[data-cy=${name}] dd`)
+  cy.getInOrder(...selectors)
+    .map('innerText')
+    .apply((values) => Cypress._.zipObject(names, values))
+    .should('deep.equal', {
+      price: '$10.99',
+      shipping: '$6.99',
+      handling: '$1.39',
+      tips: '$0.99',
+      total: '$25.36',
+    })
+})
+
+it.skip('has wrong fees (cy-spok)', () => {
+  const names = ['price', 'shipping', 'handling', 'tips', 'total']
+  const selectors = names.map((name) => `[data-cy=${name}] dd`)
+  cy.getInOrder(...selectors)
+    .map('innerText')
+    .apply((values) => Cypress._.zipObject(names, values))
+    .should(
+      spok({
+        price: '$10.99',
+        shipping: '$6.99',
+        handling: '$1.39',
+        tips: '$0.99',
+        total: '$25.36',
+      }),
+    )
+})
+
+Cypress.Commands.addQuery('difference', (expected) => {
+  const names = Object.keys(expected)
+  return (subject) => {
+    const diff = {}
+    names.forEach((name) => {
+      const actual = subject[name]
+      const expectedValue = expected[name]
+      if (actual !== expectedValue) {
+        diff[name] = { actual, expected: expectedValue }
+      }
+    })
+    return diff
+  }
+})
+
+it.skip('has wrong fees (custom difference)', () => {
+  const names = ['price', 'shipping', 'handling', 'tips', 'total']
+  const selectors = names.map((name) => `[data-cy=${name}] dd`)
+  cy.getInOrder(...selectors)
+    .map('innerText')
+    // partially apply the zip callback
+    .apply(Cypress._.zipObject.bind(null, names))
+    .difference({
+      price: '$10.99',
+      shipping: '$6.99',
+      handling: '$1.39',
+      tips: '$0.99',
+      total: '$25.36',
+    })
+    .should('be.empty')
 })
